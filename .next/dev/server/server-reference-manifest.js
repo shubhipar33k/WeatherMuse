5c9fbfba2b492a564102ac9859915c0c0f011289
shubhipareek <shubhi@enterprisebot.org>
2026-02-21 16:13:49 +0100

    chore: add .gitignore to exclude .next build artifacts and env files

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"fPqYDPpKAWjRYxQZOUCU7jo9d8L8SBi48nLpOfPr2X8=\"\n}"
--- .next/dev/static/chunks/be60f_next_dist_client_components_builtin_global-error_778dabf8.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/be60f_next_dist_94736137._.js"
-],
-    source: "dynamic"
-});
